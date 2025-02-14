dbec7c92442473364f1ad14cd9fe9a8aeffbccb0
n-simpkin <166037672+n-simpkin@users.noreply.github.com>
2025-02-14 14:32:03 +0000

    temp iteration through the decimal

--- a26recipriocalCycle2.py
+++ a26recipriocalCycle2.py
@@ -1,3 +1,24 @@
 #New, going for a fully like top down plan type approach before coding.
 for i in range(1,21):
     print(i, 1/i)
+
+for num in range(1,10):
+    cycle = 1/num
+    seen = dict()
+    digitPosition = -1
+    for digit in str(cycle): #divide by 10 every time sort of thing (more efficient than string conversion)
+        digitPosition += 1
+        if digit in seen:
+            prevInstanceLocation = 0
+        else:
+            seen[digit] = digitPosition
+
+
+#Could I implement seen as a dictionary so then I don't have to search for the last occurence
+test = {2:4, 3:2}
+print(test)
+test[2] = 5
+test[6] = 7
+print(test)
+
+        
